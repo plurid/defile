24625fbd4a668a1b1924e0fa9cf10b1cd96110c7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-23 20:25:18 +0200

    defile-explorer > dev: assets > file-icon

--- packages/defile-explorer/source/renderer/modules/assets/icons/file-icon.tsx
+++ packages/defile-explorer/source/renderer/modules/assets/icons/file-icon.tsx
@@ -13,7 +13,17 @@ const FileIcon: React.FC<FileIconProperties> = (properties) => {
         extension,
     } = properties;
 
-    console.log(extension);
+    const handleExtensionText = (extension: string) => {
+        let extensionText = extension.replace('.', '').toUpperCase();
+
+        if (extensionText.length > 3) {
+            return extensionText.slice(0, 3);
+        }
+
+        return extensionText;
+    }
+
+    const extensionText = handleExtensionText(extension);
 
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
@@ -30,7 +40,7 @@ const FileIcon: React.FC<FileIconProperties> = (properties) => {
                         }
 
                         .cls-3 {
-                            font-size: 250px;
+                            font-size: 200px;
                             fill: #fff;
                             font-family: Ubuntu-Bold, Ubuntu;
                             font-weight: 700;
@@ -55,14 +65,21 @@ const FileIcon: React.FC<FileIconProperties> = (properties) => {
                     <path className="cls-1" d="M169.55,46.16H574.68v253.9H830.45V953.84H169.55ZM731.9,447.07H266.29c.61,11.38,1.11,42.24,1.67,52.63H731.9ZM268,665.07H733.68c-.76-12-1.3-41.78-1.89-51.18H268Zm0,146.87H733.22c-.51-11.54-.92-42.17-1.36-52.14H267.93Z"/>
                     <path className="cls-1" d="M620.23,254.14V75.05l178,179.09Z"/>
                 </g>
-                <g>
-                    <rect className="cls-2" x="396.5" y="474" width="584" height="314"/>
-                    <text className="cls-3" transform="translate(445.88 726.26)">
-                        <tspan className="cls-4">E</tspan>
-                        <tspan className="cls-5" x="156.49" y="0">X</tspan>
-                        <tspan x="331.74" y="0">T</tspan>
-                    </text>
-                </g>
+
+                {extensionText && (
+                    <g>
+                        <rect className="cls-2" x="396.5" y="474" width="584" height="314"/>
+                        <text
+                            className="cls-3"
+                            transform="translate(445.88 726.26)"
+                        >
+                            {extensionText}
+                            {/* <tspan className="cls-4">E</tspan>
+                            <tspan className="cls-5" x="156.49" y="0">X</tspan>
+                            <tspan x="331.74" y="0">T</tspan> */}
+                        </text>
+                    </g>
+                )}
             </g>
         </svg>
     );
