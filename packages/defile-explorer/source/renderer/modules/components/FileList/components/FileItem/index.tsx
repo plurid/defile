4d7354fd2b19f20abc4ebdc184c07be8ba686a6e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-23 22:09:23 +0200

    defile-explorer > dev: FileList > FileItem

--- packages/defile-explorer/source/renderer/modules/components/FileList/components/FileItem/index.tsx
+++ packages/defile-explorer/source/renderer/modules/components/FileList/components/FileItem/index.tsx
@@ -43,7 +43,7 @@ const FileItem: React.FC<FileItemOwnProperties> = (properties) => {
 
     return (
         <PluridLink
-            page={`/${filepath}/${file}`}
+            page={`${filepath}${file.name}`}
             devisible={true}
             style={{
                 display: 'block',
