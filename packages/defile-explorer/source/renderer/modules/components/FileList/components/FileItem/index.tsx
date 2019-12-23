7c444406aa08a97d352c9853294862ef18c195b7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-23 18:20:38 +0200

    defile-explorer > dev: FileList > FileItem

---
+++ packages/defile-explorer/source/renderer/modules/components/FileList/components/FileItem/index.tsx
@@ -0,0 +1,51 @@
+import {
+    Dirent,
+} from 'fs';
+
+import React, {
+    // useState,
+} from 'react';
+
+import {
+    StyledFileItem,
+    StyledFileItemIcon,
+    StyledFileItemName,
+} from './styled';
+
+import DirectoryIcon from '../../../../assets/icons/directory-icon';
+import FileIcon from '../../../../assets/icons/file-icon';
+
+
+
+interface FileItemOwnProperties {
+    file: Dirent,
+}
+
+const FileItem: React.FC<FileItemOwnProperties> = (properties) => {
+    const {
+        file,
+    } = properties;
+
+    const isFile = file.isFile();
+    const isDirectory = file.isDirectory();
+
+    return (
+        <StyledFileItem>
+            <StyledFileItemIcon>
+                {isDirectory
+                    ? (<DirectoryIcon />)
+                    : isFile
+                        ? (<FileIcon />)
+                        : ''
+                }
+            </StyledFileItemIcon>
+
+            <StyledFileItemName>
+                {file.name}
+            </StyledFileItemName>
+        </StyledFileItem>
+    );
+}
+
+
+export default FileItem;
---
+++ packages/defile-explorer/source/renderer/modules/components/FileList/components/FileItem/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+
+
+export const StyledFileItem: any = styled.div`
+    display: flex;
+`;
+
+
+export const StyledFileItemIcon: any = styled.div`
+    svg {
+        height: 15px;
+        width: 15px;
+    }
+`;
+
+
+export const StyledFileItemName: any = styled.div`
+
+`;
