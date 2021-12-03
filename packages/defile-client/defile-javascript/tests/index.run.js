2bd3231a66c383b2a324a16775039ee9308bb690
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-12-03 09:59:14 +0200

    defile-client/defile-javascript > test: index

--- packages/defile-client/defile-javascript/tests/index.run.js
+++ packages/defile-client/defile-javascript/tests/index.run.js
@@ -8,17 +8,44 @@ const Defile = require('../distribution').default;
 
 runner(
     async (
-        _check,
+        check,
     ) => {
         const token = '__TEST__';
         const resource = 'name';
-        // const resource = 'id';
 
         const defile = new Defile(
             token,
         );
 
         const stream = await defile.get(resource);
+        check('has stream', !!stream, true);
+        if (!stream) {
+            return;
+        }
+
+        const file = fs.createWriteStream('./tests/file.png');
+
+        stream.pipe(file);
+    },
+);
+
+
+runner(
+    async (
+        check,
+    ) => {
+        const token = '__TEST__';
+        const resource = 'id';
+
+        const defile = new Defile(
+            token,
+        );
+
+        const stream = await defile.get(resource);
+        check('has stream', !!stream, true);
+        if (!stream) {
+            return;
+        }
 
         const file = fs.createWriteStream('./tests/file.png');
 
@@ -33,7 +60,25 @@ runner(
         _check,
     ) => {
         const token = '__TEST__';
-        // const file = 'text data file';
+        const file = 'text data file';
+
+        const defile = new Defile(
+            token,
+        );
+
+
+        const saved = await defile.save(file, {
+            // name: 'filename',
+        });
+    },
+);
+
+
+runner(
+    async (
+        _check,
+    ) => {
+        const token = '__TEST__';
         const file = fs.createReadStream('./tests/file.png');
 
         const defile = new Defile(
